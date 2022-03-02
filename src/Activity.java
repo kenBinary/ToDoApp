b556f08856267a5f6ee0b33556f8e14e205d6ae2
thennek0 <kennethbecaro0@gmail.com>
2022-03-02 14:36:37 +0800

    added activity class

---
+++ src/Activity.java
@@ -0,0 +1,29 @@
+public class Activity {
+    public String activityName;
+    public String dueDate;
+    public String dueHour;
+    public boolean finished;
+
+    Activity(String cActivityName, String cDueDate,String cDueHour,boolean cfinished){
+        activityName = cActivityName;
+        dueDate = cDueDate;
+        dueHour = cDueHour;
+        finished = cfinished;
+    }
+
+    public boolean isFinished() {
+        System.out.println(finished ? finished:false);
+        return finished ? finished:false;
+    }
+
+    public String toString() {
+        return activityName+","+dueDate+","+dueHour+","+finished;
+    }
+    
+    public boolean finished() {
+        return finished = true;
+    }
+    
+    
+
+}

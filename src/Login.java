0c46fbb933bbf393bc77b843034c09a96291ecca
thennek0 <kennethbecaro0@gmail.com>
2022-03-11 17:57:15 +0800

    update

--- src/Login.java
+++ src/Login.java
@@ -1,7 +1,7 @@
+import java.awt.*;
 import javax.swing.*;
-
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
+import javax.swing.UIManager.LookAndFeelInfo;
 
 
 public class Login{
@@ -10,72 +10,121 @@ public class Login{
     
     JFrame newFrame = new JFrame("Log in");
     JPanel jp1 = new JPanel();
+    jp1.setLayout(new GridLayout(6,1));
+    JLabel loginTitle = new JLabel("User Login Form",SwingConstants.CENTER);
+    loginTitle.setFont(new Font("Serif", Font.PLAIN, 20));
+    loginTitle.setBorder(BorderFactory.createEmptyBorder(5, 10, 15, 10));
     JLabel userName = new JLabel("User Name");
-    JTextField userName1 = new JTextField("Enter Name", 16);
-    JPasswordField passwordField = new JPasswordField(16);
-    JLabel passwordField1 = new JLabel("Password");
-    JButton submit = new JButton("Submit");
-    JButton resetButton = new JButton("RESET");
+    userName.setFont(new Font("Serif", Font.PLAIN, 15));
+    userName.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 0));
+    JTextField userName1 = new JTextField("");
+    JLabel passwordField = new JLabel("Password");
+    passwordField.setFont(new Font("Serif", Font.PLAIN, 15));
+    passwordField.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 0));
+    JPasswordField passwordField1 = new JPasswordField();
+
+
+
+    JPanel bottomPanel = new JPanel();
+    bottomPanel.setLayout(new GridLayout(1,3));
+    JButton submit = new JButton("Login");
+    JButton resetButton = new JButton("Reset");
     JCheckBox showPassword = new JCheckBox("Show Password");
+    bottomPanel.add(submit);
+    bottomPanel.add(resetButton);
+    bottomPanel.add(showPassword);
+
+ 
+    jp1.add(loginTitle);
     jp1.add(userName);
     jp1.add(userName1);
     jp1.add(passwordField);
     jp1.add(passwordField1);
-    jp1.add(submit);
 
+    resetButton.addActionListener(new ActionListener(){
+
+        @Override
+        public void actionPerformed(ActionEvent f) {
+            try {
+                if (f.getSource() == resetButton) {
+                    userName1.setText("");
+                    passwordField1.setText("");
+                }
+        
+            } catch (Exception e) {
+            }
+        }
+
+    });
+
+    showPassword.addActionListener(new ActionListener(){
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                if (e.getSource() == showPassword) {
+                    if (showPassword.isSelected()) {
+                        passwordField1.setEchoChar((char) 0);
+                    } else {
+                        passwordField1.setEchoChar('*');
+                    }
+         
+         
+                }
+            } catch (Exception f) {
+            }
+            
+        }
+        
+    });
     
     submit.addActionListener(new ActionListener() {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            newFrame.dispose();
 
 
+                String userNames = userName1.getText();
+                String pwd = new String(passwordField1.getPassword());
+                System.out.println(userNames);
+                System.out.println(pwd);
 
-
-            if (e.getSource() == submit) {
-                String userText;
-                String pwdText;
-                userText = userName1.getText();
-                pwdText = passwordField1.getText();
-
-                if (userText.equalsIgnoreCase("User") && pwdText.equalsIgnoreCase("12345")) {
-                    JOptionPane.showMessageDialog(showPassword, this, "Login Successful", 0);
-                        // MAO NI SYA ANG MU TAWAG SA APP NGA CLASS DAYUN IYANG I PAKITA
-                        // AYUHA ANG IF STATEMENT NGA MO MO GANA
+                if (userNames.equals("user") && pwd.equals("12345")) {
+                   JOptionPane.showMessageDialog(null, "Login Sucessful");
+                        newFrame.dispose();
                         App test = new App();
                         test.setVisible(true);
                 } else {
-                    JOptionPane.showMessageDialog(showPassword, this, "Invalid Username or Password", 0);
+                    JOptionPane.showMessageDialog(null, "Invalid name or Password");
                 }
-     
-            }
-           
-            if (e.getSource() == resetButton) {
-                userName1.setText("");
-                passwordField.setText("");
-            }
-    
-            if (e.getSource() == showPassword) {
-                if (showPassword.isSelected()) {
-                    passwordField.setEchoChar((char) 0);
-                } else {
-                    passwordField.setEchoChar('*');
-                }
-     
-     
-            }
         }
  
     });
-    newFrame.add(jp1);
+    newFrame.setLocationRelativeTo(null);
+    newFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    newFrame.add(jp1,BorderLayout.CENTER);
+    newFrame.add(bottomPanel,BorderLayout.SOUTH);
     newFrame.setVisible(true);
-    newFrame.setSize(500,500);
+    newFrame.setMinimumSize(new Dimension(370,250));
+    newFrame.setSize(370,250);
 
 
 }
 public static void main(String[] args) {
+    javax.swing.SwingUtilities.invokeLater(new Runnable() {
+        public void run() {
+            try {
+                for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                    if ("Nimbus".equals(info.getName())) {
+                        UIManager.setLookAndFeel(info.getClassName());
+                        break;
+                    }
+                }
+            } catch (Exception e) {
+            }
+            new Login();
+        }
+    });;
 
-    new Login();
 }
 }
